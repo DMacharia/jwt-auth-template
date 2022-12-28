81e2866fb5a43bb1aab5f3b8472a592222f0a70f
Daniel Macharia <danielhuios@gmail.com>
2022-12-28 21:02:37 +0300

    added login action in users controller and login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,17 @@ class UsersController < ApplicationController
         else
             render json: {error: "Invalid username or password"}, status: :unprocessable_entity
         end
+    end
 
-
+    def login
+        @user = User.find_by(username: user_params[:username])
+       
+        if @user && @user.authenticate(user_params[:password])
+            token  = encode_token({user_id: @user.id})
+            render json: {user: UserSerializer.new(@user), jwt: token}, status: :accepted
+       else
+            render json: {error: "Invalid username or password"}, status: :unauthorized
+       end
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resource :users, only: [:create]
+
+  post "/login", to: "users#login"
 end
